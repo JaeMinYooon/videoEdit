a1cf2f44819a8b0299b83a725dcf4af2f342bfd4
jaemin <yjm8440@naver.com>
2020-03-05 00:30:09 +0900

    color-master

--- ex1.py
+++ ex1.py
@@ -28,15 +28,18 @@ def hex_to_hsv(hexCode):
     #print(hsv)
     return hsv
 
-def color(hexCode="00688B",cutImage=""):
+def color(hexCode="#00688B",cutImage=""):
 
+    hex = hexCode.lstrip(('#'))
+    '''
     hsv = hex_to_hsv(hexCode)# hex값 넣을 곳
-    '''print("h = " + str(hsv[0][0][0]))
+    print("h = " + str(hsv[0][0][0]))
     print("s = " + str(hsv[0][0][1]))
-    print("v = " + str(hsv[0][0][2]))'''
+    print("v = " + str(hsv[0][0][2]))
     h = int(hsv[0][0][0])
     s = int(hsv[0][0][1])
     v = int(hsv[0][0][2])
+    '''
 
     #img = cv2.imread('color.png')
     #img = cv2.imread('sample.jpg')
@@ -46,12 +49,58 @@ def color(hexCode="00688B",cutImage=""):
     #lower = np.array([0, 0, 255]) # 직접 넣을 때 사용 할 코드
     #upper = np.array([0, 0, 255])
 
-    if(h==0 | s==0):
-        lower = np.array([0, 0, v])  # 직접 넣을 때 사용 할 코드
-        upper = np.array([0, 0, v])
+    if (hex == str('ff0000')):  # 빨강색YES  (핑크도 읽음)
+        lower = np.array([161, 100, 100])
+        upper = np.array([179, 255, 255])
+    elif (hex == str('ff8400')):  # 주황색 YES (노랑 조금 읽음)
+        lower = np.array([0, 180, 186])
+        upper = np.array([47, 255, 255])
+    elif (hex == str('6e4129')):  # 갈색 YES (피부 읽음)
+        lower = np.array([-15, 100, 90])
+        upper = np.array([15, 200, 255])
+    elif (hex == str('c9b56c')):  # 베이지 YES (피부 읽음)
+        lower = np.array([4, 0, 90])  # 10 5 90
+        upper = np.array([30, 120, 255])  # 30 120 210
+    elif (hex == str('ffe600')):  # 노랑 YES
+        lower = np.array([20, 100, 100])  # 4 0 90
+        upper = np.array([40, 255, 255])  # 30 255 255
+    elif (hex == str('0022ff')):  # 파랑 YES
+        lower = np.array([80, 40, 40])
+        upper = np.array([126, 255, 255])
+    elif (hex == str('252069')): # 남색 YES
+        lower = np.array([80, 40, 40])
+        upper = np.array([126, 255, 255])
+    elif (hex == str('00eeff')): # 하늘 YES
+        lower = np.array([80, 40, 40])
+        upper = np.array([126, 255, 255])
+    elif (hex == str('a805fa')):  # 보라 YES
+        lower = np.array([120, 50, 80])
+        upper = np.array([173, 215, 255])
+    elif (hex == str('f1b0ff')):  # 연보라 YES
+        lower = np.array([120, 50, 80])
+        upper = np.array([173, 215, 255])
+    elif (hex == str('ff69b4')):  # 핑크 YES
+        lower = np.array([120, 50, 80])
+        upper = np.array([173, 215, 255])
+    elif (hex == str('26ff00')):  # 초록 YES
+        lower = np.array([45, 25, 60])  # |10 0 60 | 60 100 40
+        upper = np.array([80, 255, 255])  # | 92 255 255 | 90 255 255
+    elif (hex == str('828282')):  # 회색 YES
+        lower = np.array([0, 0, 60])  # 0 0 70
+        upper = np.array([180, 43, 220])  # 180 20 255
+    elif (hex == str('ffffff')):  # 하얀 YES
+        lower = np.array([0, 0, 170])  # 0 0 70
+        upper = np.array([180, 40, 255])  # (179)180 20 255
+    elif (hex == str('000000')):  # 검정 YES
+        lower = np.array([0, 0, 0])  # 0 0 0
+        upper = np.array([179, 30, 100])  # 179 255 50
+    elif (hex == str('2a5c35')):  # 카키 YES
+        lower = np.array([10, 70, 80])  #
+        upper = np.array([40, 160, 190])  #
     else:
-        lower = np.array([h-20, 40, 40]) # hsv값 뽑은 거 이용 할 코드
-        upper = np.array([h+20, 255, 255])
+        print("input error")
+        lower = np.array([0, 0, 0])
+        upper = np.array([0, 0, 0])
 
     mask = cv2.inRange(hsv, lower, upper)
     p = percent(mask)
