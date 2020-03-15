828987b639e1a983761cf50924c55a6abfb804f5
jaemin <yjm8440@naver.com>
2020-03-16 01:26:58 +0900

    update

--- videoMake/clothClassification.py
+++ videoMake/clothClassification.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # tensorflow와 tf.keras를 임포트합니다
-
-from tensorflow import keras
 import tensorflow as tf
+from tensorflow import keras
+
 # 헬퍼(helper) 라이브러리를 임포트합니다
 import numpy as np
 import matplotlib.pyplot as plt
@@ -42,15 +42,12 @@ IMG_SIZE_Y = 160
 def create_model(class_num, load_weights_path):
     inputs = keras.Input(shape=(IMG_SIZE_X, IMG_SIZE_Y, 3))
 
-    # model = keras.applications.ResNet50(input_tensor=inputs, include_top=False, weights=None, pooling='max')
     model = keras.applications.InceptionResNetV2(input_tensor=inputs, include_top=False, weights=None, pooling='avg')
     x = model.output
     x = keras.layers.Dense(1000, name='fully')(x)
     x = keras.layers.BatchNormalization()(x)
     x = keras.layers.Activation('relu')(x)
     outputs = keras.layers.Dense(class_num, activation='softmax', name='softmax')(x)
-
-    # return keras.Model(inputs, outputs)
 
     model = keras.Model(inputs, outputs)
     model.compile(optimizer='adam',
@@ -76,43 +73,31 @@ def m_imread(path):
     return img
 
 # type 인자가 긴팔 반팔인지 구분하는 변수
-def predict(img, type, flag=0):
+def predict(img, input_type, flag=0):
+    img = cv2.resize(img, dsize=(IMG_SIZE_X, IMG_SIZE_Y), interpolation=cv2.INTER_AREA)
     img = np.expand_dims(img, axis=0)
 
     # flag = 0 : top, 1 : bottom
     if flag == 0:
         model = top_model
         classes = top_classes
+        input_type = input_type + "_top"
 
     elif flag == 1:
         model = bottom_model
         classes = bottom_classes
+        if not input_type == 'skirt':
+            input_type = input_type + "_bottom"
 
     prediction = model.predict(img)
     predict_type = classes[np.argmax(prediction)]
 
-    if type == predict_type:
+    print(predict_type)
+
+    if input_type == predict_type:
         return True
     else:
         return False
 
-# mPath = '예측할 이미지가 저장된 디렉토리 경로'
-# mList = [file for file in os.listdir(mPath) if file.endswith(".jpg")]
-# mTest = np.zeros(shape=(len(mList), IMG_SIZE_X, IMG_SIZE_Y, 3))
-#
-# for i in range(len(mList)):
-#     mTest[i] = m_imread(mPath + str(mList[i]))
-#
-# print(mTest.shape)
-#
-# predictions = top_model.predict(mTest)
-#
-# for j in range(int(len(mTest))):
-#     plt.figure(figsize=(10, 10))
-#     plt.subplot(6, 5, j + 1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(mTest[j], cmap=plt.cm.binary)
-#     plt.xlabel(top_classes[np.argmax(predictions[j])])
-#     plt.show()
+img = m_imread("C:/Users/a0102/Downloads/validation/validation/long_bottom/000290.jpg")
+print(predict(img, input_type="long_bottom", flag=1))
